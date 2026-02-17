23119330edc2f57053799d6020826c2417ae9596
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-02-17 17:51:25 +0530

    adding New files

---
+++ sets.py
@@ -0,0 +1,16 @@
+collection = {1,2,3,4,4,5}
+
+print(type(collection))
+print(collection)
+
+# to write  empty set
+emppty =set()
+
+emppty.add(3)
+emppty.add(6)
+emppty.add(8)
+
+emppty.remove(3)
+
+emppty.clear()
+print(emppty)
